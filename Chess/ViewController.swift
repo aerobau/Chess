cbc4de2dcdb03d3750ffd658548201bb31f553d5
Alexander Robau <aerobau@gmail.com>
2015-08-12 11:01:14 -0400

    Initial Commit

---
+++ Chess/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Alexander Robau on 8/12/15.
+//  Copyright (c) 2015 Alexander Robau. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
